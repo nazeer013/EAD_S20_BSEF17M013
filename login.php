9be567d4c28c5a8b6a2b0ef4a95ce01ccfa36201
nazeer013 <bsef17m013@pucit.edu.pk>
2020-03-01 17:40:51 +0500

    1.3.2020

--- login.php
+++ login.php
@@ -1,12 +1,14 @@
 <html>
 <head>
         <title> ****USER LOGIN**** </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
         <link rel="stylesheet" type= "text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css ">
 </head>
 <body>
     <div class='container'>
+        <div class= 'login-box'>
         <div class='row'>
-           <div class ="col-md-6">
+           <div class ="col-md-6 login-left">
                 <h2> Login Here </h2>
                 <form action validation="validation.php" method="post">
                     <div class="from-group">
@@ -20,10 +22,23 @@
                     <button type="submit" class="btn btn-primary"> Login </button>
                 </form>    
             </div>
+            <div class ="col-md-6 login-right">
+                <h2> Sign-Up Here </h2>
+                <form action validation="signup.php" method="post">
+                    <div class="from-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                    <div class="from-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required>
+                     </div>
+                    <button type="submit" class="btn btn-primary"> Sign-Up </button>
+                </form>    
+            </div>
+        </div>
         </div>    
     </div>
-
-
 </body>
 
 
