a64b59d1e6dcad1736160995c1ca4001a1d1b1c3
nazeer013 <60933095+nazeer013@users.noreply.github.com>
2020-04-29 21:27:44 +0500

    Done

---
+++ TokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class TokenGenerator
+    {
+        
+        public static String GetToken(string login,string password)
+        {
+            string key = "my_Name_is_Khan_&_I_Am_Not_A_Terrorist";
+            var issuer = "http://mysite.com";
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var permClaims = new List<Claim>();
+            permClaims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            permClaims.Add(new Claim("valid", "1"));
+            permClaims.Add(new Claim("login", login));
+            permClaims.Add(new Claim("Password", password));
+
+
+            var token = new JwtSecurityToken(issuer,
+                            issuer,
+                            permClaims,
+                            expires: DateTime.Now.AddDays(1),
+                            signingCredentials: credentials);
+            var jwt_token = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt_token;
+        }
+    }
+}
